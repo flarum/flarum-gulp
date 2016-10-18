e2e42bf3858dc5a6bacc8f79e211e31fc02e573f
Davis <connor0095@gmail.com>
2016-10-17 21:35:15 -0500

    Add lint task

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 var cached = require('gulp-cached');
@@ -71,5 +72,12 @@ module.exports = function(options) {
         }
       });
     }
+  });
+
+  gulp.task('lint', () => {
+      return gulp.src(['src/**/*.js'])
+          .pipe(eslint())
+          .pipe(eslint.format())
+          .pipe(eslint.failAfterError());
   });
 };
